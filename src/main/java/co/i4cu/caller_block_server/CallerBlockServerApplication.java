a2a124f31009550b5f3120e6625c927b04784e1b
btquan <i4s33u@protonmail.com>
2022-09-17 15:28:55 +0700

    Initial commit

---
+++ src/main/java/co/i4cu/caller_block_server/CallerBlockServerApplication.java
@@ -0,0 +1,13 @@
+package co.i4cu.caller_block_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallerBlockServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallerBlockServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/i4cu/caller_block_server/CallerBlockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.i4cu.caller_block_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallerBlockServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
